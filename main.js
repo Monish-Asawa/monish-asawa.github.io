6d7f724e81fb801605118c6c16249a5d64032044
monish-asawa <monishasawa@gmail.com>
2023-07-23 22:26:58 +0530

    more responsiveness

---
+++ main.js
@@ -0,0 +1,30 @@
+// et id1 = document.getElementById("1")
+// let id2 = document.getElementById("2")
+// let id3 = document.getElementById("3")
+
+// function homecontentin() {
+//     id1. = "rgba(53, 120, 171, 1 )"
+//     id2. = "rgba(53, 120, 171, 1 )"
+//     id3. = "rgba(53, 120, 171, 1 )"
+// }
+// function homecontentout() {
+//     id1.style.boxShadow = "none"
+//     id2.style.boxShadow = "none"
+//     id3.style.boxShadow = "none"
+// }
+// function aboutcontentin() {
+//     id1. = "rgba(54, 50, 255) "
+//     id2. = "rgba(54, 50, 255) "
+//     id3. = "rgba(54, 50, 255) "
+// }
+// function skillscontentin() {
+//     id1. = "rgba(2, 127, 119)"
+//     id2. = "rgba(2, 127, 119)"
+//     id3. = "rgba(2, 127, 119)"
+// }
+// function resumecontentin() {
+//     id1. = "rgba(53, 120, 171, 1 )"
+//     id2. = "rgba(53, 120, 171, 1 )"
+//     id3. = "rgba(53, 120, 171, 1 )"
+// }
+
